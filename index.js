a4c8665a28318e945acc652596919b31fd7529d5
Bikal <bikaladhikari5@gmail.com>
2024-04-17 16:52:28 +1000

    project completion

--- index.js
+++ index.js
@@ -21,6 +21,19 @@ function passwordCheck(req, res, next) {
   next();
 }
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+  if (userIsAuthorised) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.sendFile(__dirname + "/public/index.html");
+    //Alternatively res.redirect("/");
+  }
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
